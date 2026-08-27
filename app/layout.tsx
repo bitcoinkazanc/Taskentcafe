f9afb24284cde26d0ba7f7e25f732026463b6aed
bitcoinkazanc <bitcoinkazanc@gmail.com>
2026-08-27 23:05:40 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
- import "./globals.css";
+import "./globals.css";
 import BottomNav from "../components/BottomNav";
 
 export const metadata = {
@@ -15,7 +15,6 @@ export default function RootLayout({
     <html lang="tr">
       <body>
         {children}
-
         <BottomNav />
       </body>
     </html>
